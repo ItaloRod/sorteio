ab6c2a0dc705a9da0e3155b1a9b1ef3a932b63b0
Paulo <pauloitalo@hotmail.com.br>
2018-02-04 14:00:45 -0300

    add na amostragem dos dados

--- View.py
+++ View.py
@@ -17,15 +17,18 @@ class Application(tk.Frame):
         self.quitButton.grid(padx=2,pady=2)
 
     def sortear (self):
-        print (ganhadores)
         self.label.delete(0,'end')
         sorteado = random.choice(li)
         if sorteado in ganhadores:
-            sorteado = random.choice(li)
+            self.sortear()
         else:
+
             ganhadores.append(sorteado)
             self.label.insert(0, "Parabéns: %s " % str(sorteado))
+            print (str(ganhadores.index(ganhadores[-1])+1) + "º Prêmio | Ganhador: "+ str(ganhadores[-1]))
 
+
+print ('*** LISTA DE GANHADORES ***')
 li = np.genfromtxt("indicados.txt",delimiter=',',dtype='str')
 ganhadores = []
 app = Application()
